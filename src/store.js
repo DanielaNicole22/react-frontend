255f7d0704e18d953f4deb9dcebb19c59e581dd5
unknown <rebatis.nicole@gmail.com>
2023-01-08 08:04:51 +0800

    added comments

--- src/store.js
+++ src/store.js
@@ -3,8 +3,18 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
+/**
+ * A module for creating store using redux
+ */
+
+/**
+ * Middleware that allows to return functions, rather than just actions, within Redux
+ */
 const middleware = [thunk];
 
+/**
+ * Creates a Redux store that holds the complete state tree of the app
+ */
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middleware))
